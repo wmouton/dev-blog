7f8af78ae4df5eb59f77b622dadd9f8268529129
l33th <l33th@protonmail.ch>
2021-09-27 08:43:01 +0200

    React Blog with Title no Background

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -4,13 +4,26 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typeography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+// import { fontSize } from "@mui/system";
 
 const Bar = () => {
 	const useStyles = makeStyles((themes) => ({
 		appBar: {
 			backgroundColor: "#fff",
 		},
-
+		hero: {
+			backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('../img/glassCodeHack.jpg')`,
+			height: "500px",
+			backgroundPosition: "center",
+			backgroundRepeat: "no-repeat",
+			backgroundSize: "cover",
+			position: "relative",
+			display: "flex",
+			justifyContent: "center",
+			alignItems: "center",
+			color: "#fff",
+			fontSize: "4rem",
+		},
 	}));
 
 	const classes = useStyles();
@@ -20,16 +33,14 @@ const Bar = () => {
 			<div>
 				<AppBar className={classes.appBar} position='static'>
 					<Toolbar>
-						<Typeography variant='h6'>
+						<Typeography variant='h6' color='default'>
 							Blog
 						</Typeography>
 					</Toolbar>
 				</AppBar>
-                <Box className={classes.hero}>
-                    <Box>
-                        React Blog
-                    </Box>
-                </Box>
+				<Box className={classes.hero}>
+					<Box>React Blog</Box>
+				</Box>
 			</div>
 		</>
 	);
